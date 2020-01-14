2b7f093e321df00df32f5eb5bdb947e56e6094a8
mohitthakur007 <41528050+mohitthakur007@users.noreply.github.com>
2020-01-14 08:35:25 +0530

    Create 2.1 Express usage to send html text.js

---
+++ 2.1 Express usage to send html text.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const port =8000;
+var app=express();
+app.get('/',function(req,res){
+    res.send("hiiiiii!!!!!")
+});
+
+app.listen(port,function(err){
+    if(err){
+        console.log("ERROR",err);
+    }
+    console.log("Server is running at port ",port);
+});
