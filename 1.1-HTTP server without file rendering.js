9f267ba2fa9f25971975ccca2152af0168d4a259
mohitthakur007 <41528050+mohitthakur007@users.noreply.github.com>
2020-01-15 06:48:04 +0530

    Update 1.1-HTTP server without file rendering.js

--- 1.1-HTTP server without file rendering.js
+++ 1.1-HTTP server without file rendering.js
@@ -1,3 +1,5 @@
+//This will show loading in the browser but server will start running because request is not handled.
+
 const http= require('http');
 const port=8000;
 var server=http.createServer();
